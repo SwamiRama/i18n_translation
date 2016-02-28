2e8b59b33f554ed3514bc7075a65e980e3d5183a
Jimmi <jimmi@rl-hosting.de>
2016-02-28 22:57:53 +0100

    fast setup

--- lib/i18n_translation/file.rb
+++ lib/i18n_translation/file.rb
@@ -1,35 +1,43 @@
 require 'fileutils'
+module I18nTranslation
+  module Translate
+    class File
+      attr_accessor :path
 
-class Translate::File
-  attr_accessor :path
-  
-  def initialize(path)
-    self.path = path
-  end
-  
-  def write(keys)
-    FileUtils.mkdir_p File.dirname(path)
-    File.open(path, "w") do |file|
-      file.puts keys_to_yaml(Translate::File.deep_stringify_keys(keys))
-    end    
-  end
-  
-  def read
-    File.exists?(path) ? YAML::load(IO.read(path)) : {}
-  end
+      def initialize(path)
+        self.path = path
+      end
 
-  # Stringifying keys for prettier YAML
-  def self.deep_stringify_keys(hash)
-    hash.inject({}) { |result, (key, value)|
-      value = deep_stringify_keys(value) if value.is_a? Hash
-      result[(key.to_s rescue key) || key] = value
-      result
-    }
+      def write(keys)
+        FileUtils.mkdir_p File.dirname(path)
+        File.open(path, 'w') do |file|
+          file.puts keys_to_yaml(Translate::File.deep_stringify_keys(keys))
+        end
+      end
+
+      def read
+        File.exist?(path) ? YAML.load(IO.read(path)) : {}
+      end
+
+      # Stringifying keys for prettier YAML
+      def self.deep_stringify_keys(hash)
+        hash.inject({}) do |result, (key, value)|
+          value = deep_stringify_keys(value) if value.is_a? Hash
+          result[(begin
+                    key.to_s
+                  rescue
+                    key
+                  end) || key] = value
+          result
+        end
+      end
+
+      private
+
+      def keys_to_yaml(keys)
+        # Using ya2yaml, if available, for UTF8 support
+        keys.respond_to?(:ya2yaml) ? keys.ya2yaml(escape_as_utf8: true) : keys.to_yaml
+      end
+    end
   end
-  
-  private
-  def keys_to_yaml(keys)
-    # Using ya2yaml, if available, for UTF8 support
-    keys.respond_to?(:ya2yaml) ? keys.ya2yaml(:escape_as_utf8 => true) : keys.to_yaml
-  end    
 end
--- lib/i18n_translation/storage.rb
+++ lib/i18n_translation/storage.rb
@@ -1,28 +1,33 @@
-class Translate::Storage
-  attr_accessor :locale
-  
-  def initialize(locale)
-    self.locale = locale.to_sym
-  end
-  
-  def write_to_file
-    Translate::File.new(file_path).write(keys)
-  end
-  
-  def self.file_paths(locale)
-    Dir.glob(File.join(root_dir, "config", "locales", "**","#{locale}.yml"))
-  end
-  
-  def self.root_dir
-    Rails.root
-  end
-  
-  private
-  def keys
-    {locale => I18n.backend.send(:translations)[locale]}
-  end
-  
-  def file_path
-    File.join(Translate::Storage.root_dir, "config", "locales", "#{locale}.yml")
+module I18nTranslation
+  module Translate
+    class Storage
+      attr_accessor :locale
+
+      def initialize(locale)
+        self.locale = locale.to_sym
+      end
+
+      def write_to_file
+        Translate::File.new(file_path).write(keys)
+      end
+
+      def self.file_paths(locale)
+        Dir.glob(File.join(root_dir, 'config', 'locales', '**', "#{locale}.yml"))
+      end
+
+      def self.root_dir
+        Rails.root
+      end
+
+      private
+
+      def keys
+        { locale => I18n.backend.send(:translations)[locale] }
+      end
+
+      def file_path
+        File.join(Translate::Storage.root_dir, 'config', 'locales', "#{locale}.yml")
+      end
+    end
   end
 end

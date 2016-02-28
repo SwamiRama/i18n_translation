0e226029fef95a69d25bbd238bf990e3f3fd2439
Jimmi <jimmi@rl-hosting.de>
2016-02-28 22:41:01 +0100

    fast setup

--- lib/i18n_translation.rb
+++ lib/i18n_translation.rb
@@ -6,7 +6,3 @@ module I18nTranslation
   # For configuring Bing Application id
   mattr_accessor :app_id
 end
-
-Dir[File.join(File.dirname(__FILE__), "translate", "*.rb")].each do |file|
-  require file
-end

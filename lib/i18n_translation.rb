d5859703ed2c59e3bf272534877ca1b6d94d9c49
Jimmi <jimmi@rl-hosting.de>
2016-02-28 22:40:02 +0100

    fast setup

--- lib/i18n_translation.rb
+++ lib/i18n_translation.rb
@@ -1,4 +1,12 @@
-require "i18n_translation/engine"
+require 'i18n_translation/engine'
 
 module I18nTranslation
+  # For configuring Google Translate API key
+  mattr_accessor :api_key
+  # For configuring Bing Application id
+  mattr_accessor :app_id
+end
+
+Dir[File.join(File.dirname(__FILE__), "translate", "*.rb")].each do |file|
+  require file
 end
--- lib/i18n_translation/engine.rb
+++ lib/i18n_translation/engine.rb
@@ -1,5 +1,6 @@
 module I18nTranslation
   class Engine < ::Rails::Engine
     isolate_namespace I18nTranslation
+    require 'translate'
   end
 end

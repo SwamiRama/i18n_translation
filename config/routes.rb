ec2d135f1babca7e7113cd629e3d9d0094989069
Manuel Reischl <manuel.reischl@modomoto.de>
2016-02-29 10:56:03 +0100

    debugging

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 I18nTranslation::Engine.routes.draw do
-  get 'translate/index'
+  # get 'translate/index'
+  # get 'translate/translate'
 
-  get 'translate/translate'
-
-  # get 'translate' => 'translate#index', :as => :translate_list
-  # post 'translate' => 'translate#translate', :as => :translate
-  # get 'translate/reload' => 'translate#reload', :as => :translate_reload
+  get 'translate' => 'translate#index', :as => :translate_list
+  post 'translate' => 'translate#translate', :as => :translate
+  get 'translate/reload' => 'translate#reload', :as => :translate_reload
 end

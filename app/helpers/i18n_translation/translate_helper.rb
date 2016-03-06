224f99bf8b08cf50bff19751dd524252bf739581
Jimmi <jimmi@linux.speedport.ip>
2016-03-06 16:04:15 +0100

    move filter to navigation bar

--- app/helpers/i18n_translation/translate_helper.rb
+++ app/helpers/i18n_translation/translate_helper.rb
@@ -1,6 +1,6 @@
 module I18nTranslation
   module TranslateHelper
-    def simple_filter(labels, param_name = 'filter', selected_value = nil)
+    def sort_by_filter(labels, param_name = 'filter', selected_value = nil)
       selected_value ||= params[param_name]
       filter = []
       labels.each do |item|
@@ -10,14 +10,14 @@ module I18nTranslation
           type = label = item
         end
         if type.to_s == selected_value.to_s
-          filter << "<div class='btn btn-success btn-block'>#{label}</div>"
+          filter << "<a class='btn btn-success'>#{label}</a>"
         else
           link_params = params.merge(param_name.to_s => type)
           link_params['page'] = nil if param_name.to_s != 'page'
-          filter << link_to(label, link_params, {class: 'btn btn-default btn-block'})
+          filter << link_to(label, link_params, {class: 'btn btn-default'})
         end
       end
-      filter.join('</br> ')
+      filter.join(' ')
     end
 
     def n_lines(text, line_size)
